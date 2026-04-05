08ea1846964908dcdefe7c6037692ae3bd0a5815
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-04-05 17:22:42 +0600

    Working on structure and setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Mapend } from "./engine/mapend"
+import { useAppContext } from "./hook/use-app-context"
 import MAdapter from "./structure/madapter"
 import {ENV_KEY, envValue, MConfig } from "./structure/mconfig"
 import MRegistry from "./structure/mregistry"
@@ -12,5 +13,6 @@ export {
     MAdapter,
     envValue,
     ENV_KEY,
-    Mapend
+    Mapend,
+    useAppContext
 }
